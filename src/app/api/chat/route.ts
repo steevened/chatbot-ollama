fe54d5290a58fa2bfacea536423901d1cc80b334
steevened <alvaradosteven315@gmail.com>
2025-01-10 17:53:34 -0500

    handle tools calling with openai api and ollama

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -1,30 +1,90 @@
-import {
-  AIMessage,
-  HumanMessage,
-  SystemMessage,
-} from "@langchain/core/messages";
-import { ChatOllama } from "@langchain/ollama";
-import { LangChainAdapter, type Message } from "ai";
+import { createOpenAI } from "@ai-sdk/openai";
+import { streamText, tool, type Message } from "ai";
+import { z } from "zod";
+// import { openai } from "@ai-sdk/openai";
+// import OpenAI from "openai";
+// import {} from "@ai-sdk/openai";
+
+// const openai = new OpenAI({
+//   baseURL: "http://localhost:11434/v1",
+//   apiKey: "ollama",
+// });
 
 const PROMPT = `you're a helpful assistant`;
 
-const systemMessage = new SystemMessage(PROMPT);
+const openai = createOpenAI({
+  // custom settings, e.g.
+  // compatibility: "strict", // strict mode, enable when using the OpenAI API
+  baseURL: "http://localhost:11434/v1",
+  apiKey: "ollama",
+});
 
 export async function POST(req: Request) {
-  const { messages }: { messages: Message[] } = await req.json();
+  const { messages } = await req.json();
 
-  const model = new ChatOllama({
-    checkOrPullModel: true,
-    model: "mistral",
-    temperature: 0.1,
+  // const result = await openai.chat.completions.create({
+  //   model: "llama3.1",
+  //   stream: true,
+  //   messages,
+  //   // tools: [
+  //   //   {
+  //   //     type: "function",
+  //   //     function: {
+  //   //       function: getCurrentLocation,
+  //   //       parameters: { type: "object", properties: {} },
+  //   //     },
+  //   //   },
+  //   //   {
+  //   //     type: "function",
+  //   //     function: {
+  //   //       function: getWeather,
+  //   //       parse: JSON.parse, // or use a validation library like zod for typesafe parsing.
+  //   //       parameters: {
+  //   //         type: "object",
+  //   //         properties: {
+  //   //           location: { type: "string" },
+  //   //         },
+  //   //       },
+  //   //     },
+  //   //   },
+  //   // ],
+  // });
+
+  const result = streamText({
+    model: openai("llama3.1"),
+    // prompt: PROMPT,
+    messages,
+    tools: {
+      weather: tool({
+        description: "Get the weather in a location (fahrenheit)",
+        parameters: z.object({
+          location: z.string().describe("The location to get the weather for"),
+        }),
+        execute: async ({ location }) => {
+          const temperature = Math.round(Math.random() * (90 - 32) + 32);
+          return {
+            location,
+            temperature,
+          };
+        },
+      }),
+    },
   });
 
-  const result = await model.stream([
-    // systemMessage,
-    ...messages.map((m) =>
-      m.role === "user" ? new HumanMessage(m.content) : new AIMessage(m.content)
-    ),
-  ]);
-
-  return LangChainAdapter.toDataStreamResponse(result);
+  for await (const textPart of result.textStream) {
+    console.log(JSON.stringify(textPart));
+  }
+  return result.toDataStreamResponse();
 }
+
+// async function getCurrentLocation() {
+//   return "Boston"; // Simulate lookup
+// }
+
+// async function getWeather(args: { location: string }) {
+//   const { location } = args;
+//   // … do lookup …
+//   const temperature = 123;
+//   const precipitation = 123;
+//   return { temperature, precipitation };
+// }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,8 @@ import { useChat } from "ai/react";
 export default function ChatbotUI() {
   const { messages, input, handleInputChange, handleSubmit } = useChat();
 
+  console.log(messages);
+
   return (
     <div className="flex flex-col h-screen bg-gray-100">
       <div className="flex-1 overflow-y-auto p-4">
